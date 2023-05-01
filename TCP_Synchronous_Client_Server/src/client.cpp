41e37e955d4d0f05703712a6e2caf238fad54ee2
Rachit Garg <rachitgarg900@gmail.com>
2023-05-01 14:57:51 +0200

    Added TCP_Synchronous_Client_Server code

--- TCP_Synchronous_Client_Server/src/client.cpp
+++ TCP_Synchronous_Client_Server/src/client.cpp
@@ -6,6 +6,50 @@ using ip::tcp;
 
 int main()
 {
-    std::cout << "Client starting...." << std::endl;
+    std::cout << "Starting Client..." << std::endl;
+
+    // io_service object needed for core io functionality of asio
+    io_service io_service_;
+
+    // Create a socket
+    tcp::socket socket_{io_service_};
+
+    std::cout << "Connecting to server..." << std::endl;
+
+    // Connect the client to server endpoint on local host ipv4 on port 1234
+    socket_.connect(tcp::endpoint(ip::address::from_string("127.0.0.1"), 1234));
+
+    std::cout << "Connected to server!" << std::endl;
+
+    // Defining this for storing error codes
+    boost::system::error_code error;
+
+    // Send message to server
+    std::string msg;
+    std::cout << "Type the message you wanna send." << std::endl;
+    std::getline(std::cin, msg);
+    msg += "\n";
+    write(socket_, buffer(msg), error);
+    if (!error)
+    {
+        std::cout << "Message sent to server successfully!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Message could not be sent! " << error.message() << std::endl;
+    }
+
+    // Getting the response message back from the server
+    streambuf buf;
+    read(socket_, buf, transfer_all(), error);
+    if (error && error != error::eof)
+    {
+        std::cout << "Could not receive message from the server! " << error.message() << std::endl;
+    }
+    else
+    {
+        std::cout << "Received message from the server: " << buffer_cast<const char*>(buf.data());
+    }
+
     return 0;
 }
--- TCP_Synchronous_Client_Server/src/server.cpp
+++ TCP_Synchronous_Client_Server/src/server.cpp
@@ -4,8 +4,48 @@
 using namespace boost::asio;
 using ip::tcp;
 
+std::string receive_(tcp::socket& socket) 
+{
+    // read until a newline character is found and return the contents as a C++ string
+    streambuf buf;
+    read_until(socket, buf, "\n");
+    std::string data = buffer_cast<const char*>(buf.data());
+    return data;
+}
+
+void send_(tcp::socket& socket, const std::string& message)
+{
+    // Append a newline character to the message and send on the socket
+    const std::string msg = message + "\n";
+    write(socket, buffer(msg));
+}
+
 int main()
 {
-    std::cout << "Server starting...." << std::endl;
+    std::cout << "Starting Server..." << std::endl;
+
+    // io_service object needed for core io functionality of asio
+    io_service io_service_;
+    
+    // Create accepter object to listen for incoming connection on ipv4 localhost address (127.0.0.1)
+    tcp::acceptor acceptor_(io_service_, tcp::endpoint(tcp::v4(), 1234));
+
+    // Create a socket object
+    tcp::socket socket_{io_service_};
+
+    std::cout << "Waiting for a client to connect..." << std::endl;
+
+    // Waiting for client to connect
+    acceptor_.accept(socket_);
+
+    std::cout << "Client connected!" << std::endl;
+
+    // Read data from client once it is connected
+    std::string data = receive_(socket_);
+    std::cout << "Received message from client: " << data;
+
+    // Write data
+    send_(socket_, "Hello from your lovely Server!");
+    std::cout << "Hello Message sent to client!" << std::endl;
     return 0;
 }
