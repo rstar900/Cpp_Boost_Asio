38bce4b7abb57792ce098db379f418945cdc7716
Rachit Garg <rachitgarg900@gmail.com>
2023-05-01 13:11:59 +0200

    Created Basic Project Structure

---
+++ TCP_Synchronous_Client_Server/src/client.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace boost::asio;
+using ip::tcp;
+
+int main()
+{
+    std::cout << "Client starting...." << std::endl;
+    return 0;
+}
